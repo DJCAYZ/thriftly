4fcf3d8333846ff795ea685fdc575adfaef93215
DJCAYZ <charlesaaron07@gmail.com>
2025-02-02 20:32:32 +0800

    remove dark mode and update guest layout

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -4,14 +4,14 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+        <div className="flex flex-col min-h-screen items-center bg-gray-100 pt-6 sm:pt-0">
+            <header className="absolute self-start mt-2">
+                <Link href='/'>
+                    <ApplicationLogo className="h-20 w-20 fill text-gray-500" />
                 </Link>
-            </div>
+            </header>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
+            <div className="my-auto w-full h-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
         </div>
