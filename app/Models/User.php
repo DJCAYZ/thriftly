e964277e004d8fb1ef3bf38e367512a559f4909c
DJCAYZ <charlesaaron07@gmail.com>
2025-03-25 17:06:38 +0800

    Create Account model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,5 +53,9 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function googleUser(): HasOne {
         return $this->hasOne(GoogleUser::class, 'user_id');
+    }
+
+    public function accounts(): HasMany {
+        return $this->hasMany(Account::class);
     }
 }
