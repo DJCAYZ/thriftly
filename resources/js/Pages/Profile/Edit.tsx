b54c9c7d215830e0c1bc61210b9a815d21a6dd51
DJCAYZ <charlesaaron07@gmail.com>
2025-03-28 14:09:03 +0800

    Remove unnecessary padding in profile page

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -17,33 +17,28 @@ export default function Edit({
             title="Edit Profile"
         >
             <Head title="Profile" />
+            <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                <TwoFactorAuthForm
+                    status={status}
+                    hasEnabledTwoFactorAuth={hasEnabledTwoFactorAuth}
+                    className="max-w-xl"
+                />
+            </div>
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <TwoFactorAuthForm
-                            status={status}
-                            hasEnabledTwoFactorAuth={hasEnabledTwoFactorAuth}
-                            className="max-w-xl"
-                        />
-                    </div>
+            <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                <UpdateProfileInformationForm
+                    mustVerifyEmail={mustVerifyEmail}
+                    status={status}
+                    className="max-w-xl"
+                />
+            </div>
 
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <UpdateProfileInformationForm
-                            mustVerifyEmail={mustVerifyEmail}
-                            status={status}
-                            className="max-w-xl"
-                        />
-                    </div>
+            <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                <UpdatePasswordForm className="max-w-xl" />
+            </div>
 
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <UpdatePasswordForm className="max-w-xl" />
-                    </div>
-
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
-                </div>
+            <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                <DeleteUserForm className="max-w-xl" />
             </div>
         </AuthenticatedLayout>
     );
