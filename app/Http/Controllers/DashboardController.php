0826ff190045b386d3813da5245dea57d56cfa47
DJCAYZ <charlesaaron07@gmail.com>
2025-05-17 13:53:04 +0800

    add another column in group by

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -39,7 +39,7 @@ class DashboardController extends Controller
                         ['transactions.created_at', '>=', 'UNIX_TIMESTAMP(DATE(NOW() - INTERVAL 30 DAY))'],
                     ])
                     ->join('transaction_categories', 'transactions.category_id', '=', 'transaction_categories.id')
-                    ->groupBy('transaction_categories.id')
+                    ->groupBy('transactions.category_id', 'transaction_categories.name')
                     ->orderBy('amount', 'desc')
                     ->select(
                         'transaction_categories.name',
